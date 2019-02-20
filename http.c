87065ed12e9ebf65bce928555691736bc4503132
edward <kupei@ucsc.edu>
2019-02-19 17:53:33 -0800

    init version

---
+++ http.c
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2018 David C. Harrison - All Rights Reserved.
+ * You may not use, distribute, or modify this code without
+ * the express written permission of the copyright holder.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#define BYTES 2048
+
+static void binary(int sock, char *fname) {
+    int fd;
+    int bytes;
+    void *buffer[BYTES];
+    if ((fd = open(fname, O_RDONLY)) != -1) {
+        while ((bytes = read(fd, buffer, BYTES)) > 0)
+            write(sock, buffer, bytes);
+   }
+}
+
+void httpRequest(int sock, char *request) { }
+
